31792b3ecbd2b4db0f3ca996c1b8d5b857ff59c5
watanabe0055 <mailok1212+github@gmail.com>
2023-01-05 01:40:37 +0900

    fix

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,10 +1,5 @@
-/* eslint-disable @next/next/no-img-element */
-import moment from 'moment'
-import Link from 'next/link'
 import client from '../../../libs/client'
-// eslint-disable-next-line import/no-unresolved
-import Icon from '@/atom/icon/icon'
-// eslint-disable-next-line import/no-unresolved
+import AppConteiner from '@/atom/conteiner/conteiner'
 import BlogShow from '@/template/id/index'
 import type { Blog, Category } from '@/type/blog'
 
@@ -42,7 +37,9 @@ export default function BlogId({ blog }: any) {
   console.log(blog)
   return (
     <>
-      <BlogShow title={blog.title} content={blog.content} updatedAt={blog.updatedAt} />
+      <AppConteiner>
+        <BlogShow title={blog.title} content={blog.content} updatedAt={blog.updatedAt} />
+      </AppConteiner>
     </>
   )
 }
--- src/template/id/index.tsx
+++ src/template/id/index.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 // eslint-disable-next-line import/no-unresolved
 import styled from './index.module.scss'
 //import AppConteiner from '@/atom/conteiner/conteiner'
+// eslint-disable-next-line import/no-unresolved
+import Button from '@/atom/button/buttons'
 
 const BlogShow = (props: { title: string; content: string; updatedAt: string }) => {
   const { title, content, updatedAt } = props
@@ -17,7 +19,7 @@ const BlogShow = (props: { title: string; content: string; updatedAt: string }) 
         />
         <p> 最終更新日: {moment(updatedAt).format('YYYY-MM-DD')}</p>
         <Link href={`/`}>
-          <button>一覧へ</button>
+          <Button text={'一覧へ'} />
         </Link>
       </div>
     </>
